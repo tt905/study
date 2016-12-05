1793ad0e6b8b9009ce6786fcf286aa88ec66a1f7
motingwei <tingwei.mo@itseye.com>
2016-12-05 17:12:06 +0800

    日期选择

---
+++ studyApp/src/main/java/com/mo/study/MainActivity2.java
@@ -0,0 +1,57 @@
+package com.mo.study;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.mo.study.ui.RecyclerViewActivity;
+
+public class MainActivity2 extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn1).setOnClickListener(this);
+//        findViewById(R.id.btn2).setOnClickListener(this);
+//        findViewById(R.id.btn3).setOnClickListener(this);
+//        findViewById(R.id.btn4).setOnClickListener(this);
+//        findViewById(R.id.btn5).setOnClickListener(this);
+//        findViewById(R.id.btn6).setOnClickListener(this);
+//        findViewById(R.id.btn7).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        Intent intent = new Intent();
+
+        switch (v.getId()){
+
+            case R.id.btn1:
+                intent.setClass(this, RecyclerViewActivity.class);
+                break;
+//            case R.id.btn2:
+//                intent.setClass(this, PieActivity.class);
+//                break;
+//            case R.id.btn3:
+//                intent.setClass(this, PActivity.class);
+//                break;
+//            case R.id.btn4:
+//                intent.setClass(this, PathActivity.class);
+//                break;
+//            case R.id.btn5:
+//                intent.setClass(this, BezierActivity.class);
+//                break;
+//            case R.id.btn6:
+//                intent.setClass(this, FingerActivity.class);
+//                break;
+//            case R.id.btn7:
+//                intent.setClass(this, CoordinatorActivity.class);
+//                break;
+        }
+        startActivity(intent);
+    }
+}
---
+++ studyApp/src/main/java/com/mo/study/ui2/DatePickActivity.java
@@ -0,0 +1,20 @@
+package com.mo.study.ui2;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.mo.study.R;
+
+/**
+ * Created by motw on 2016/12/5.
+ */
+public class DatePickActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_date_pick);
+    }
+}
