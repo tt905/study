cd4abb9b5d6a25aa783858acb5b85686b38628e0
motingwei <tingwei.mo@itseye.com>
2016-12-23 14:46:54 +0800

    loading dialog add

--- studyApp/src/main/java/com/mo/study/ui2/LoadingActivity.java
+++ studyApp/src/main/java/com/mo/study/ui2/LoadingActivity.java
@@ -3,10 +3,13 @@ package com.mo.study.ui2;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.mo.study.R;
+import com.mo.study.widget.LoadingDialog;
 
 /**
+ * loading activity
  * Created by motw on 2016/12/19.
  */
 public class LoadingActivity extends AppCompatActivity {
@@ -16,6 +19,13 @@ public class LoadingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
 
+        findViewById(R.id.btn_show_loading).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                new LoadingDialog(LoadingActivity.this).show();
+                LoadingDialog.show(LoadingActivity.this, "正在登陆");
+            }
+        });
     }
 
 
---
+++ studyApp/src/main/java/com/mo/study/widget/LoadingDialog.java
@@ -0,0 +1,80 @@
+package com.mo.study.widget;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.widget.TextView;
+
+import com.mo.study.R;
+
+/**
+ * loading dialog <br/>
+ * Created by motw on 2016/12/23.
+ */
+public class LoadingDialog extends AlertDialog {
+
+
+    private TextView text;
+
+    private String message;
+
+    private LoadingDialog(Context context) {
+        this(context, R.style.loadingStyle);
+    }
+
+    private LoadingDialog(Context context, int theme) {
+        super(context, theme);
+    }
+
+    //几个方便使用的static方法
+
+    public static LoadingDialog show(Context context, String message) {
+        return show(context, message, true, false);
+    }
+
+    public static LoadingDialog show(Context context, String message, boolean cancelable) {
+        return show(context, message, cancelable, false);
+    }
+
+    public static LoadingDialog show(Context context, String message,
+                                     boolean cancelable, boolean ousideable) {
+        LoadingDialog dialog = new LoadingDialog(context);
+        dialog.setLoadingText(message);
+        dialog.setCancelable(cancelable);
+        dialog.setCanceledOnTouchOutside(ousideable);
+        dialog.show();
+        return dialog;
+    }
+    //  end
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setCancelable(true);
+//        setCanceledOnTouchOutside(false);
+        setContentView(R.layout.layout_loading);
+
+        text = (TextView) findViewById(R.id.tv_loading_text);
+        if (message != null){
+            text.setText(message);
+        }
+    }
+
+    /**
+     * 设置文字
+     * @param msg
+     */
+    public void setLoadingText(String msg){
+        if (msg == null){
+            return ;
+        }
+
+        message = msg;
+
+        if (text != null) {
+            text.setText(msg);
+        }
+    }
+
+}
