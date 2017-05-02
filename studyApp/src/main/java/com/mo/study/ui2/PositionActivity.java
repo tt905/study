37fcec9f04430a3feb386d984fb1cf1a32058abc
motingwei <tingwei.mo@itseye.com>
2017-05-02 11:52:55 +0800

    新加使用网络定位

--- studyApp/src/main/java/com/mo/study/ui2/PositionActivity.java
+++ studyApp/src/main/java/com/mo/study/ui2/PositionActivity.java
@@ -49,9 +49,10 @@ public class PositionActivity extends AppCompatActivity implements View.OnClickL
         setContentView(R.layout.activity_position);
 
         findViewById(R.id.btn_gps_pos).setOnClickListener(this);
-        findViewById(R.id.btn_cell_pos).setOnClickListener(this);
-        findViewById(R.id.btn_wifi_pos).setOnClickListener(this);
+        findViewById(R.id.btn_cell_info).setOnClickListener(this);
+        findViewById(R.id.btn_wifi_info).setOnClickListener(this);
         findViewById(R.id.btn_gps_last).setOnClickListener(this);
+        findViewById(R.id.btn_net_pos).setOnClickListener(this);
         progressBar = (ProgressBar) findViewById(R.id.progress_gps);
 
         tvGpsInfo = (TextView) findViewById(R.id.tv_gps_info);
@@ -67,6 +68,7 @@ public class PositionActivity extends AppCompatActivity implements View.OnClickL
     protected void onDestroy() {
         super.onDestroy();
         mLocationManager.removeUpdates(locationGpsListener);
+        mLocationManager.removeUpdates(mNetLocationListener);
     }
 
     @Override
@@ -77,19 +79,72 @@ public class PositionActivity extends AppCompatActivity implements View.OnClickL
             case R.id.btn_gps_pos:
                 requestGpsPosition();
                 break;
-            case R.id.btn_cell_pos:
+            case R.id.btn_cell_info:
                 getCellTrackInfo();
                 break;
-            case R.id.btn_wifi_pos:
+            case R.id.btn_wifi_info:
                 getWiFiPosition();
                 break;
             case R.id.btn_gps_last:
                 getLastPosition();
                 break;
+            case R.id.btn_net_pos:
+                requestNetPosition();
+                break;
         }
     }
 
 
+    private void requestNetPosition(){
+        //网络定位是否可用
+        if (!mLocationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+            Toast.makeText(this, "请开启定位...", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivityForResult(intent, 0);
+            return;
+        }
+
+        mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000L, 0f, mNetLocationListener);
+
+    }
+
+    private LocationListener mNetLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            Log.d(TAG, "mNetLocationListener ----> onLocationChanged");
+
+            //只定位一次
+//            if (location != null){
+//                mLocationManager.removeUpdates(mNetLocationListener);
+//            }
+            tvGpsInfo.setText("定位成功");
+
+            StringBuilder sb = new StringBuilder();
+            sb.append("latitude: ").append(location.getLatitude()).append("\n")
+                    .append("longitude: ").append(location.getLongitude()).append("\n")
+                    .append("方向: ").append(location.getBearing()).append("\n")
+                    .append("高度: ").append(location.getAltitude()).append("\n")
+                    .append("速度: ").append(location.getSpeed()).append("\n")
+                    .append("精度: ").append(location.getAccuracy());
+            Log.d(TAG, sb.toString());
+            tvGpsInfo.setText(tvGpsInfo.getText().toString() + "\n" + sb.toString());
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
 
     /**
      * 获取GPS定位
@@ -106,7 +161,7 @@ public class PositionActivity extends AppCompatActivity implements View.OnClickL
         Toast.makeText(this, "GPS定位", Toast.LENGTH_SHORT).show();
         progressBar.setVisibility(View.VISIBLE);
         // 10秒一次，N米距离
-        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10L, 3f, locationGpsListener);
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000L, 1f, locationGpsListener);
 //        mManager.requestSingleUpdate(LocationManager.GPS_PROVIDER, locationGpsListener, null);
         //监听GPS状态，可以获取卫星信息,包括 卫星的高度角、方位角、信噪比、和伪随机号（及卫星编号）
 //        mLocationManager.addGpsStatusListener(gpsStatusListener);
